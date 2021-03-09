4a2496490898268fe2ca37e6b078805a60b032d8
Ankit Kumar Singh <ankitchbs333@gmail.com>
2021-03-09 17:46:02 +0530

    skill bar animation
    As skills section becomes visible, skills bar level increases from zero to desired level.

--- javascript_code.js
+++ javascript_code.js
@@ -1,3 +1,5 @@
+////////////smooth scroll-----start///////////////////
+
 var navMenuAnchorTags = document.querySelectorAll('nav a');
 
 var interval;
@@ -19,3 +21,51 @@ function scrollVertically(targetSection){
 	}
 	window.scrollBy(0, 50);
 }
+
+///////////smooth scroll----end //////////
+
+
+/////////skill bar auto fill----start/////////
+
+var progressBars = document.querySelectorAll('.skills-progress > div');
+var skillsContainer = document.getElementById('skills-container');
+window.addEventListener('scroll', checkScroll);
+
+function initialiseBars(){
+	for(let bar of progressBars){
+		bar.style.width = 0 + '%';
+	}
+}
+
+initialiseBars();
+
+var animationDone = false;
+
+function fillBars(){
+	for(let bar of progressBars){
+		let currLevel = 0;
+		let maxLevel = bar.dataset.barWidth;
+		let interval = setInterval(function(){
+			bar.style.width = currLevel + '%';
+			currLevel++;
+			if(currLevel>=maxLevel){
+				clearInterval(interval);
+				return;
+			}
+		}, 5);
+	}
+}
+
+function checkScroll(){
+	var coordinates = skillsContainer.getBoundingClientRect();
+	if(!animationDone && coordinates.top < window.innerHeight){
+		animationDone = true;
+		fillBars();
+	}
+	else if(animationDone && (coordinates.top > window.innerHeight)){
+		animationDone = false;
+		initialiseBars();
+	}
+}
+
+///////skill bar auto fill-----end///////////
